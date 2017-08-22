1eeed72ac4cbedcc19f72569dce5682934f60280
aiyappa-latticefund <31066221+aiyappa-latticefund@users.noreply.github.com>
2017-08-22 17:19:08 +0530

    HCPCS descriptions

--- parse.py
+++ parse.py
@@ -3,6 +3,9 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 import json
+import requests
+import time
+import sys
 
 ailment = None
 lastSeenCode = None
@@ -13,8 +16,10 @@ col_names = ['ALLOWED CHARGES', 'ALLOWED SERVICES', 'PAYMENT']
 def setAilment(row):
 	global ailment
 	try:
+		print row['DESCRIPTION']
 		if str(row['DESCRIPTION']) != 'nan':
 			ailment = str(row['DESCRIPTION'])
+			print ailment
 			return True
 		else:
 			return False
@@ -71,6 +76,12 @@ def getCSVs(year):
 	mypath = './data/'+year
 	return [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
+def getHCPCSFiles():
+	from os import listdir
+	from os.path import isfile, join
+	mypath = './hcpcs'
+	return [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
 def parse_csvs():
 	yearsSkipRows = {
 		'2000': 4,
@@ -91,6 +102,7 @@ def parse_csvs():
 		'2015': 3
 	}
 	for year in getYears():
+		print year
 		for file_name in getCSVs(year):
 			global ailment
 			try:
@@ -178,12 +190,104 @@ def read_json(file_name):
 	with open(file_name) as data_file:
 	    return json.load(data_file)
 
+def get_deets_for_codes(codes):
+	print len(codes)
+	deets = []
+	count = 0
+	for x in range((len(codes)/100)+1):
+		try:
+			selected = codes[(count*100):((count+1)*100)]
+			selectedCodes = ''
+			for sc in selected:
+				selectedCodes+=(sc+',')
+			hipaaspaceToken='182191EA884C497893E9A574312C08D7559DC87E7F4E48EEA1A8CC0E649292AC'
+			payload = {'q': selectedCodes, 'rt': 'json', 'token': hipaaspaceToken}
+			# print payload
+			r = requests.get('https://www.hipaaspace.com/api/hcpcs/getcodes', params=payload)
+			# print r.json()
+			deets += r.json()['HCPCS']
+			count+=1
+			if count%10 == 0:
+				time.sleep(60)
+		except:
+			print 'EXCEPTION! on request ', count+1
+			print deets
+			time.sleep(60)
+	print 'Request: ',count
+	print len(deets)
+	print 'Coverage', float(len(deets))/float(len(codes))*100
+	return deets
+
+def get_hcpcs_deets():
+	codes = []
+	for key in ailmentToCode['2010'].keys():
+		codes += [str(code) for code in ailmentToCode['2010'][key]]
+	sortedCodes = list(set(codes))
+	total = len(sortedCodes)
+	selected = []
+	selected+=sortedCodes[(total/3):(total/3)+10]
+	selected+=sortedCodes[(2*total/3):(2*total/3)+10]
+	print selected
+	get_deets_for_codes(selected)
+
 # parse_csvs()
 # with open('ailmentToCode.json', 'w') as outfile:
 #     json.dump(ailmentToCode, outfile)
 # with open('codeToVal.json', 'w') as outfile:
 #     json.dump(codeToVal, outfile)
-ailmentToCode = read_json('./ailmentToCode.json')
-codeToVal = read_json('./codeToVal.json')
-line_data = build_ailment_aggregate()
-generate_line(line_data)
+# ailmentToCode = read_json('./ailmentToCode.json')
+# codeToVal = read_json('./codeToVal.json')
+# line_data = build_ailment_aggregate()
+# print sorted([str(key) for key in ailmentToCode['2010'].keys()])
+# print sorted([str(key) for key in ailmentToCode['2009'].keys()])
+# get_hcpcs_deets()
+# hcpcsDeets = read_json('./hcpcs.json')
+
+# for ailment in ailmentToCode['2015'].keys():
+# 	print ailment, len(set([str(code) for code in ailmentToCode['2015'][ailment]]))
+
+# year = '2015'
+# for ailment in ailmentToCode[year].keys():
+# 	if ailment != 'CARDIOVASCULAR':
+# 		print ailment
+# 		codes = list(set([str(code) for code in ailmentToCode[year][ailment]]))
+# 		deets = get_deets_for_codes(codes)
+# 		ailment = ailment.replace('/', ' ')
+# 		with open(ailment+'.json', 'w') as outfile:
+# 			json.dump({'hcpcs':deets}, outfile)
+# 		time.sleep(60)
+
+# generate_line(line_data)
+
+wordMap = {}
+
+sentence = 'I have to buy contact lens and I need to call an ambulance to fix my fractured bone and get family planning'
+
+for file_name in getHCPCSFiles():
+	# print file_name
+	try:
+		currJson = read_json('./hcpcs/'+file_name)
+		for code in currJson['hcpcs']:
+			for word in sentence.split(' '):
+				if not word in wordMap:
+					wordMap[word] = {'ailments':{}}
+				if word in code['LongDescription'].split(' ') or word in code['ShortDescription'].split(' '):
+					# print word
+					# print code['HCPC'], code['LongDescription'], code['ShortDescription']
+					if not file_name in wordMap[word]['ailments']:
+						wordMap[word]['ailments'][file_name] = {'codes':set()}
+					if not code['HCPC'] in wordMap[word]['ailments'][file_name]['codes']:
+						# print 'Adding ',code['HCPC']
+						wordMap[word]['ailments'][file_name]['codes'].add(code['HCPC'])
+	except:
+		print 'Error processing ', file_name, sys.exc_info()
+		raise
+
+# print wordMap
+for word in wordMap:
+	for ailment in wordMap[word]['ailments']:
+		print word, ailment, len(wordMap[word]['ailments'][ailment]['codes'])
+
+
+
+
